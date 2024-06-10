97835d1541f04c0a95a5b738710f776eaad5dee0
mail@lenblum.de <mail@lenblum.de>
2024-06-10 11:34:19 +0200

    added support for admin script in upload file

--- upload.sh
+++ upload.sh
@@ -1,3 +1,5 @@
 #upload.sh <remote_user>@<remote_ip>
 cargo build --release
 scp target/release/$(basename $(pwd)) $1:~/sasbackend/$(basename $(pwd))
+(cd admin_script; go build admin.go)
+scp admin_script/admin $1:~/sasbackend/admin
