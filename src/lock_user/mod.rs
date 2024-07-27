4299da8ec8c1282d50420d45b2fb9e5b5935351a
mail@lenblum.de <mail@lenblum.de>
2024-07-27 12:25:14 +0200

    use RwLock instead of Mutex

--- src/lock_user/mod.rs
+++ src/lock_user/mod.rs
@@ -1,8 +1,9 @@
 use std::sync::Arc;
 
+use tokio::sync::{RwLock};
+
 use once_cell::sync::Lazy;
 use salvo::conn::SocketAddr;
-use tokio::sync::Mutex;
 
 struct User {
     ip: SocketAddr,
@@ -13,11 +14,11 @@ struct Users {
     users: Vec<User>,
 }
 
-static TEST_VCTR: Lazy<Arc<Mutex<Users>>> =
-    Lazy::new(|| Arc::new(Mutex::new(Users { users: Vec::new() })));
+static LOCKED_USERS: Lazy<Arc<RwLock<Users>>> =
+    Lazy::new(|| Arc::new(RwLock::new(Users { users: Vec::new() })));
 
 pub async fn increment_failed_attempts(ip: SocketAddr) {
-    let mut users = TEST_VCTR.lock().await;
+    let mut users = LOCKED_USERS.write().await;
     let mut found = false;
     for user in users.users.iter_mut() {
         if matches!(&user.ip, ip) {
@@ -26,7 +27,7 @@ pub async fn increment_failed_attempts(ip: SocketAddr) {
                 // start unlock timer
                 tokio::spawn(async move {
                     tokio::time::sleep(std::time::Duration::from_secs(60 * 5)).await;
-                    let mut users = TEST_VCTR.lock().await;
+                    let mut users = LOCKED_USERS.write().await;
                     users.users.retain(|u| !matches!(&u.ip, ip));
                 });
             }
@@ -43,7 +44,7 @@ pub async fn increment_failed_attempts(ip: SocketAddr) {
 }
 
 pub async fn is_locked(ip: SocketAddr) -> bool {
-    let users = TEST_VCTR.lock().await;
+    let users = LOCKED_USERS.read().await;
     for user in users.users.iter() {
         if matches!(&user.ip, ip) {
             return user.failed_attempts >= 3;
@@ -53,6 +54,6 @@ pub async fn is_locked(ip: SocketAddr) -> bool {
 }
 
 pub async fn unlock(ip: SocketAddr) {
-    let mut users = TEST_VCTR.lock().await;
+    let mut users = LOCKED_USERS.write().await;
     users.users.retain(|u| !matches!(&u.ip, ip));
 }
