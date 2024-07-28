48b11f19849706c8d78e3c3bbce053128ad756bc
mail@lenblum.de <mail@lenblum.de>
2024-07-28 10:27:07 +0200

    reset your failed attempts after 20 minutes no matter what

--- src/lock_user/mod.rs
+++ src/lock_user/mod.rs
@@ -8,6 +8,7 @@ use salvo::conn::SocketAddr;
 struct User {
     ip: SocketAddr,
     failed_attempts: u8,
+    is_resetting: bool,
 }
 
 struct Users {
@@ -23,22 +24,23 @@ pub async fn increment_failed_attempts(ip: SocketAddr) {
     for user in users.users.iter_mut() {
         if matches!(&user.ip, ip) {
             user.failed_attempts += 1;
-            if user.failed_attempts >= 3 {
-                // start unlock timer
-                tokio::spawn(async move {
-                    tokio::time::sleep(std::time::Duration::from_secs(60 * 15)).await;
-                    let mut users = LOCKED_USERS.write().await;
-                    users.users.retain(|u| !matches!(&u.ip, ip));
-                });
+                if !user.is_resetting {
+                    user.is_resetting = true;
+                    tokio::spawn(async move {
+                        tokio::time::sleep(std::time::Duration::from_secs(60 * 20)).await;
+                        let mut users = LOCKED_USERS.write().await;
+                        users.users.retain(|u| !matches!(&u.ip, ip));
+                    });    
+                }
             }
             found = true;
             break;
         }
-    }
     if !found {
         users.users.push(User {
             ip,
             failed_attempts: 1,
+            is_resetting: false,
         });
     }
 }
